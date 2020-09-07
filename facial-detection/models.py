6893aa918792c522cbdcf81980189293032346af
hyungkyu <hyungkyu@deepeyes.co.uk>
2020-09-07 22:08:29 +0100

    loss and optimisation with multi convolution

--- facial-detection/models.py
+++ facial-detection/models.py
@@ -12,27 +12,49 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        
+
         ## TODO: Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-        
+
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
-        
+        output_size1 = int((28 - 5) / 1 + 1)
+
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        self.pool = nn.MaxPool2d(2, 2)
 
-        # [hyungkyu] define a pooling layer
-        self.pool = nn.MaxPool2d(5, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        output_size2 = int((output_size1 - 5) / 1 + 1)
 
-        
+        in_features = 64 * output_size2 * output_size2
+        out_features = int((in_features + 13) / 2)
+        # self.fc1 = nn.Linear(in_features, out_features)
+        # self.fc1_drop = nn.Dropout(p=0.4)
+        # self.fc2 = nn.Linear(out_features, 13)
+
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         x = self.pool(F.relu(self.conv1(x)))
-        
+        x = self.pool(F.relu(self.conv2(x)))
+
+        # prep for linear layer
+        # flatten the inputs into a vector
+        x = x.view(x.size(0), -1)
+        #
+        # # one linear layer
+        # x = F.relu(self.fc1(x))
+        # x = self.fc1_drop(F.relu(self.fc1(x)))
+        # # a softmax layer to convert the 10 outputs into a distribution of class scores
+        x = F.log_softmax(x, dim=1)
+
+        # x = self.fc2(x)
+        # x = F.log_softmax(x)
+
         # a modified x, having gone through all the layers of your model, should be returned
         return x
+
--- facial-detection/training_data.py
+++ facial-detection/training_data.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 import cv2
 
@@ -49,11 +50,10 @@ from data_load import Rescale, RandomCrop, Normalize, ToTensor
 #                         transforms.ToTensor(),
 #         ])
 
-data_transform = transforms.Compose([
-    Rescale(256),
-    RandomCrop(224),
-    ToTensor()
-])
+data_transform = transforms.Compose([Rescale(256),
+                                     RandomCrop(28),
+                                     Normalize(),
+                                     ToTensor()])
 
 # testing that you've defined a transform
 assert (data_transform is not None), 'Define a data_transform'
@@ -63,7 +63,7 @@ transformed_dataset = FacialKeypointsDataset(csv_file='data/training_frames_keyp
                                              root_dir='data/training/',
                                              transform=data_transform)
 
-print('Number of images: ', len(transformed_dataset))
+print('Number of training images: ', len(transformed_dataset))
 
 # iterate through the transformed dataset and print some stats about the first few samples
 for i in range(4):
@@ -83,6 +83,8 @@ test_dataset = FacialKeypointsDataset(csv_file='data/test_frames_keypoints.csv',
                                       root_dir='data/test/',
                                       transform=data_transform)
 
+print('Number of test images: ', len(test_dataset))
+
 # load test data in batches
 batch_size = 10
 
@@ -92,8 +94,15 @@ test_loader = DataLoader(test_dataset,
                          num_workers=0)
 
 
-# test the model on a batch of test images
+# for i, sample in enumerate(test_loader):
+#     images = sample['image']
+#     key_pts = sample['keypoints']
+#     images = images.type(torch.FloatTensor)
+#     output_pts = net(images)
+    # output_pts = output_pts.view(output_pts.data.size()[0], 68, -1)
+    # print(images.data.size(), key_pts.data.size(), output_pts.data.size())
 
+# test the model on a batch of test images
 def net_sample_output():
     # iterate through the test dataset
     for i, sample in enumerate(test_loader):
@@ -109,7 +118,7 @@ def net_sample_output():
         output_pts = net(images)
 
         # reshape to batch_size x 68 x 2 pts
-        output_pts = output_pts.view(output_pts.size()[0], 68, -1)
+        # output_pts = output_pts.view(output_pts.size()[0], 68, -1)
 
         # break after first image is tested
         if i == 0:
@@ -129,3 +138,103 @@ if __name__ == '__main__':
 print(test_images.data.size())
 print(test_outputs.data.size())
 print(gt_pts.size())
+
+
+def show_all_keypoints(image, predicted_key_pts, gt_pts=None):
+    """Show image with predicted keypoints"""
+    # image is grayscale
+    plt.imshow(image, cmap='gray')
+    plt.scatter(predicted_key_pts[:, 0], predicted_key_pts[:, 1], s=20, marker='.', c='m')
+    # plot ground truth points as green pts
+    if gt_pts is not None:
+        plt.scatter(gt_pts[:, 0], gt_pts[:, 1], s=20, marker='.', c='g')
+
+
+# visualize the output
+# by default this shows a batch of 10 images
+def visualize_output(test_images, test_outputs, gt_pts=None, batch_size=10):
+    for i in range(batch_size):
+        plt.figure(figsize=(20, 10))
+        ax = plt.subplot(1, batch_size, i + 1)
+
+        # un-transform the image data
+        image = test_images[i].data  # get the image from it's Variable wrapper
+        image = image.numpy()  # convert to numpy array from a Tensor
+        image = np.transpose(image, (1, 2, 0))  # transpose to go from torch to numpy image
+
+        # un-transform the predicted key_pts data
+        predicted_key_pts = test_outputs[i].data
+        predicted_key_pts = predicted_key_pts.numpy()
+        # undo normalization of keypoints
+        predicted_key_pts = predicted_key_pts * 50.0 + 100
+
+        # plot ground truth points for comparison, if they exist
+        ground_truth_pts = None
+        if gt_pts is not None:
+            ground_truth_pts = gt_pts[i]
+            ground_truth_pts = ground_truth_pts * 50.0 + 100
+
+        # call show_all_keypoints
+        # show_all_keypoints(np.squeeze(image), predicted_key_pts, ground_truth_pts)
+
+        plt.axis('off')
+
+    plt.show()
+
+
+# call it
+visualize_output(test_images, test_outputs, gt_pts)
+
+## TODO: Define the loss and optimization
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001)
+
+
+def train_net(n_epochs):
+    # prepare the net for training
+    net.train()
+
+    for epoch in range(n_epochs):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+
+        # train on batches of data, assumes you already have train_loader
+        for batch_i, data in enumerate(train_loader):
+            # get the input images and their corresponding labels
+            images = data['image']
+            key_pts = data['keypoints']
+
+            # flatten pts
+            key_pts = key_pts.view(key_pts.size(0), -1)
+
+            # convert variables to floats for regression loss
+            key_pts = key_pts.type(torch.FloatTensor)
+            images = images.type(torch.FloatTensor)
+            #
+            # # forward pass to get outputs
+            output_pts = net(images)
+            #
+            # # calculate the loss between predicted and target keypoints
+            loss = criterion(output_pts, key_pts)
+            #
+            # # zero the parameter (weight) gradients
+            optimizer.zero_grad()
+            #
+            # # backward pass to calculate the weight gradients
+            loss.backward()
+            #
+            # # update the weights
+            optimizer.step()
+            #
+            # # print loss statistics
+            running_loss += loss.item()
+            if batch_i % 10 == 9:  # print every 10 batches
+                print('Epoch: {}, Batch: {}, Avg. Loss: {}'.format(epoch + 1, batch_i + 1, running_loss / 10))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+# train your network
+n_epochs = 1 # start small, and increase when you've decided on your model structure and hyperparams
+
+train_net(n_epochs)
